2e15a05e3dffd48a90ba6f9a228b5f7cfb06d895
Daguer <dgrfps@gmail.com>
2022-07-25 18:32:30 -0300

    Update USBHostShield.ino

--- VMouse/USBHostShield.ino
+++ VMouse/USBHostShield.ino
@@ -73,7 +73,35 @@ void setup()
     HidMouse.SetReportParser(0, &Prs);
 }
 
-void loop()
-{
+void loop() {
   Usb.Task();
+
+  if(Serial.available() > 0) {
+    String rawdata = Serial.readString();
+    String _x = split(rawdata, ',', 0);
+    String _y = split(rawdata, ',', 1);
+
+    if((_x != "") && (_y != "")) {
+      int x = _x.toInt();
+      int y = _y.toInt();
+
+      Mouse.move(x, y);
+    }
+  }
+}
+
+String split(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
